887386eac6307957bd909ec710d2cf6ca5ddedce
daquicksilvercom <daquicksilver@gmail.com>
2019-07-16 17:21:51 +0000

    Final commit

---
+++ app/src/main/java/com/example/alc4phase1/AboutALCActivity.java
@@ -0,0 +1,34 @@
+package com.example.alc4phase1;
+
+import android.content.DialogInterface;
+import android.net.http.SslError;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AboutALCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+
+        WebView webView = findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://andela.com/alc/");
+    }
+}
